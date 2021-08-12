67ee3beedfa04bf820cb06ebb5de73d82056db07
Zavanven <avanven@gmail.com>
2021-08-12 12:22:37 +0200

    Added simple pipeline to store projects in sqlite database

--- kickstarter/kickstarter/pipelines.py
+++ kickstarter/kickstarter/pipelines.py
@@ -12,12 +12,44 @@ import sqlite3
 class KickstarterPipeline:
     def open_spider(self, spider):
         self.connection = sqlite3.connect('kickstarter.db')
-        self.cursor = self.connection.cursor
-        self.cursor.execute('''
-            CREATE TABLE projects(
-                
-            )
-        ''')
+        self.cursor = self.connection.cursor()
+        try:
+            self.cursor.execute('''
+                CREATE TABLE projects(
+                    project_name TEXT,
+                    project_url TEXT,
+                    description TEXT,
+                    profile_name TEXT,
+                    profile_url TEXT,
+                    category TEXT,
+                    unique (project_name)
+                )
+            ''')
+            self.connection.commit()
+        except sqlite3.OperationalError:
+            pass
 
     def process_item(self, item, spider):
+        self.cursor.execute(
+            '''
+            INSERT INTO projects (
+                project_name,
+                project_url,
+                description,
+                profile_name,
+                profile_url,
+                category
+            ) VALUES(?,?,?,?,?,?)
+            '''
+        , (
+            item.get('project_name'),
+            item.get('project_url'),
+            item.get('description'),
+            item.get('profile_name'),
+            item.get('profile_url'),
+            item.get('category'),
+        ))
         return item
+
+    def close_spider(self, spider):
+        self.connection.close()
